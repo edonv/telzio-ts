4d6741417f8b146adc96f74493567654fa31f934
Edon <edon@valdman.works>
2024-08-01 09:26:04 -0500

    Update types.ts
    Refactored method for merging components from each schema.

--- src/types.ts
+++ src/types.ts
@@ -4,4 +4,11 @@ export { webhooks } from './gen/webhooks.js';
 import { components as primaryComponents } from './gen/schema.js';
 import { components as webhookComponents } from './gen/webhooks.js';
 
-export type components = primaryComponents & webhookComponents;
+export interface components {
+    schemas: primaryComponents['schemas'] & webhookComponents['schemas'],
+    responses: webhookComponents['responses'],
+    parameters: webhookComponents['parameters'],
+    requestBodies: webhookComponents['requestBodies'],
+    headers: webhookComponents['headers'],
+    pathItems: webhookComponents['pathItems'],
+}
