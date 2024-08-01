2e8e689f952d1a8753c05ec979c22e4b2ee827b0
Edon <edon@valdman.works>
2024-08-01 09:07:05 -0500

    Added OpenAPI desc for webhooks

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,7 @@
-export * from './gen/schema.js';
+export { paths } from './gen/schema.js';
+export { webhooks } from './gen/webhooks.js';
+
+import { components as primaryComponents } from './gen/schema.js';
+import { components as webhookComponents } from './gen/webhooks.js';
+
+export type components = primaryComponents & webhookComponents;
